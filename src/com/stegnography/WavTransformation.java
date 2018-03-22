d52ccb09ad6b5599cec79982b403e3ce9729fc98
kaushikkusa <36933258+kaushikkusa@users.noreply.github.com>
2018-03-21 22:23:57 -0500

    Add files via upload

--- src/com/stegnography/WavTransformation.java
+++ src/com/stegnography/WavTransformation.java
@@ -5,18 +5,20 @@ import java.io.IOException;
 import java.util.logging.Level;
 
 import com.stegnography.compress.ImageCompressor;
+import com.stegnography.dwt.wavelets.ACBand;
 import com.stegnography.dwt.wavelets.IWT;
 import com.stegnography.dwt.wavelets.IWTCoefficients;
 import com.stegnography.dwt.wavelets.Matrix;
 import com.stegnography.dwt.wavelets.WaveletTransform;
 import com.stegnography.image.ImageDto;
 import com.stegnography.image.ImageReader;
+import com.stegnography.model.IWTContent;
 import com.stegnography.utils.Constants;
 import com.stegnography.utils.Log;
 
 public class WavTransformation {
 	private int mDecompLevels = 1;
-	private int mQuantizLevels = 64;
+	private int mQuantizLevels = 32;
 	private boolean doReconstruct;
 	private Class classWaveletTransform;
 	private String mOutputFormat = Constants.extBMP;
@@ -26,40 +28,37 @@ public class WavTransformation {
 		this.mOutputFormat = mOutputFormat;
 	}
 
-	WavTransformation(int dLvls, int quantLvls, Class transformClass) {
-		mDecompLevels = dLvls;
-		mQuantizLevels = quantLvls;
+	WavTransformation() {
 		doReconstruct = true;
-		classWaveletTransform = transformClass;
-		
+		classWaveletTransform = ACBand.class;
+
 		new File(Constants.resultsFolder).mkdirs();
 		new File(Constants.resultsFolder, Constants.picsFolder).mkdirs();
 		new File(Constants.resultsFolder, Constants.resultsDebugDataFolder).mkdirs();
 	}
 
-
-
-	public void decomposeImage(String filename) {
+	public IWTContent decomposeImage(String filename) {
 		ImageReader ia = new ImageReader();
+		IWTContent iwtContent = new IWTContent();
 		ImageDto imageData = null;
 		try {
 			imageData = ia.readImageFile(filename);
-			Log.getInstance().log(Level.FINE,
-							"Image from file " + filename + " was read(w=" + imageData.width + ", h="
-									+ imageData.height + ").");
-			// System.out.println("Image from file "+filename+" was read(w="+imageData.width+", h="+imageData.height+").");
-	
-			//if (toCopyOriginImageToResults)
-				//imageData.saveToImageFile(filename, mOutputFormat);
+			Log.getInstance().log(Level.FINE, "Image from file " + filename + " was read(w=" + imageData.width + ", h="
+					+ imageData.height + ").");
+			// System.out.println("Image from file "+filename+" was
+			// read(w="+imageData.width+", h="+imageData.height+").");
+
+			// if (toCopyOriginImageToResults)
+			// imageData.saveToImageFile(filename, mOutputFormat);
 		} catch (IOException e) {
 			e.printStackTrace();
-			return;
+			return null;
 		}
 
 		final boolean logCoefsToFile = true;
 
-		IWTCoefficients[]  dwtCoefs;
-		
+		IWTCoefficients[] dwtCoefs;
+
 		WaveletTransform method = null;
 		try {
 			method = (WaveletTransform) classWaveletTransform.newInstance();
@@ -67,63 +66,47 @@ public class WavTransformation {
 			e.printStackTrace();
 		}
 		dwtCoefs = decomposeImage(logCoefsToFile, imageData, method);
-		System.out.println(imageData.getFilename());
-		if (doReconstruct)
-			simpleReconstruct(new IWT(method), imageData.getFilename(), imageData.width, imageData.height, false, dwtCoefs);
 
-		
+		iwtContent.setDwtCoefs(dwtCoefs);
+		iwtContent.setImageData(imageData);
 
-		final String imageFilename = imageData.getFilename() + method.getCaption();
-		Log.getInstance().log(Level.INFO,
-				"\n -=Quantization=-  [" + mQuantizLevels + " levels]");
-		ImageCompressor mQuantization = new ImageCompressor(mQuantizLevels);
-		IWTCoefficients decodedCoefs[] = mQuantization.process(dwtCoefs, imageFilename);
-			
-		if (doReconstruct)
-			if (decodedCoefs != null) {
-				IWT dwt = new IWT(method);
-			
-				simpleReconstruct(dwt, imageData.getFilename(), imageData.width, imageData.height, true, decodedCoefs);
-			} else {
-				Log.getInstance().log(Level.WARNING,
-						"Reconstruction received empty image coefs");
-			}
+		return iwtContent;
+
 	}
 
-
-	 
 	private IWTCoefficients[] decomposeImage(boolean doLogCoefs, ImageDto imageData, WaveletTransform transform) {
 		// start Haar decomposition
 		IWT dwt = new IWT(transform);
-		Log.getInstance().log(
-				Level.FINE,
-				"\n" + dwt.getTranformation().getCaption()
-						+ ": \n\tHor\t\tVer\t\tDiag\t\t\tAverage");
-		IWTCoefficients[] coefs = dwt.decompose(
-				new Matrix[] {
-					new Matrix(imageData.pixelsR), 
-					new Matrix(imageData.pixelsG),
-					new Matrix(imageData.pixelsB)}, 
-					true, doLogCoefs, mDecompLevels);
+		Log.getInstance().log(Level.FINE,
+				"\n" + dwt.getTranformation().getCaption() + ": \n\tHor\t\tVer\t\tDiag\t\t\tAverage");
+		IWTCoefficients[] coefs = dwt.decompose(new Matrix[] { new Matrix(imageData.pixelsR),
+				new Matrix(imageData.pixelsG), new Matrix(imageData.pixelsB) }, true, doLogCoefs, mDecompLevels);
 		return coefs;
 	}
 
-	private void simpleReconstruct(IWT dwt, String imageFilename, int w, int h, boolean isHuffman, IWTCoefficients... coef) {
-		Log.getInstance().log(Level.FINE,
-				"\nReconstruction attempt.. (" + imageFilename + ")");
+	public void reconstruct(IWTContent dwData) {
+		WaveletTransform method = null;
+		try {
+			method = (WaveletTransform) classWaveletTransform.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		reconstruct(new IWT(method), dwData.getImageData().getFilename(), dwData.getImageData().width,
+				dwData.getImageData().height, false, dwData.getDwtCoefs());
+
+	}
+
+	private void reconstruct(IWT dwt, String imageFilename, int w, int h, boolean isHuffman, IWTCoefficients... coef) {
+		Log.getInstance().log(Level.FINE, "\nReconstruction attempt.. (" + imageFilename + ")");
 
 		Matrix reconstR = dwt.reconstruct(coef[0]);
 		Matrix reconstG = dwt.reconstruct(coef[1]);
 		Matrix reconstB = dwt.reconstruct(coef[2]);
 
-		ImageDto reconstImage = new ImageDto(reconstR.get(),
-				reconstG.get(), reconstB.get(), w, h);
-		
+		ImageDto reconstImage = new ImageDto(reconstR.get(), reconstG.get(), reconstB.get(), w, h);
 
-		reconstImage.saveToImageFile(imageFilename, mOutputFormat);	
+		reconstImage.saveToImageFile(imageFilename, mOutputFormat);
+
 	}
-
-
-
 
 }
