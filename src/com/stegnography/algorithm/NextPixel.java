51d259dd45bf1a1e1f686a18c641f1fbdb69f728
kaushikkusa <36933258+kaushikkusa@users.noreply.github.com>
2018-04-02 00:58:56 -0500

    Add files via upload

---
+++ src/com/stegnography/algorithm/NextPixel.java
@@ -0,0 +1,29 @@
+package com.stegnography.algorithm;
+
+public class NextPixel implements Comparable<NextPixel> {
+	public int[] coordinates;
+	public double cost;
+	public double gradientCost;
+	public NextPixel(double cost, int[] coordinates){
+		this.cost =cost;
+		this.coordinates = coordinates;
+	}
+	public NextPixel(double cost,double gradientCost, int[] coordinates){
+		this.cost =cost;
+		this.gradientCost = gradientCost;
+		this.coordinates = coordinates;
+	}
+
+	public int compareTo(NextPixel other){
+		if( cost < other.cost){
+			return -1;
+		}else{ 
+			if( cost > other.cost){ 
+				return +1;
+			}else{
+				return 0;
+			}
+		}
+	}
+
+}
